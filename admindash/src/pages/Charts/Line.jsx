30848ca65d102a4299970ec590457cc88cb0b51a
sleepiCat <tyuen003@gmail.com>
2022-08-03 17:16:07 -0700

    complete structure of react component files

---
+++ admindash/src/pages/Charts/Line.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+
+// import { ChartsHeader, LineChart } from '../../components';
+
+// const Line = () => (
+//   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+//     <ChartsHeader category="Line" title="Inflation Rate" />
+//     <div className="w-full">
+//       <LineChart />
+//     </div>
+//   </div>
+// );
+
+// export default Line;
+
+import React from "react";
+
+const Line = () => {
+	return <div>Line</div>;
+};
+
+export default Line;
---
+++ admindash/src/pages/Charts/Stacked.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+
+// import { ChartsHeader, Stacked as StackedChart } from '../../components';
+
+// const Stacked = () => (
+//   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+//     <ChartsHeader category="Stacked" title="Revenue Breakdown" />
+//     <div className="w-full">
+//       <StackedChart />
+//     </div>
+//   </div>
+// );
+
+// export default Stacked;
+
+import React from "react";
+
+const Stacked = () => {
+	return <div>Stacked</div>;
+};
+
+export default Stacked;
